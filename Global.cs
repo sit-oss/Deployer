75189c191dd9924fef8256037a59f54dbcaa5516
TsungKang <admin@ccc.xxx>
2025-04-22 14:30:05 +0900

    Feat: Add action RunAndWait

--- Global.cs
+++ Global.cs
@@ -54,6 +54,7 @@ namespace Deployer
                     var splDownloadFont = target.Split(new[] { '|' }, 3);
                     if (splDownloadFont.Length != 3) return false;
                     var windowsFontsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Microsoft", "Windows", "Fonts");
+                    if (!Directory.Exists(windowsFontsPath)) Directory.CreateDirectory(windowsFontsPath);
                     var fontPath = Path.Combine(windowsFontsPath, Path.GetFileName(splDownloadFont[0]));
                     if(File.Exists(fontPath)) return true;
                     var downloadFont = await DownloadFile(splDownloadFont[0]);
@@ -94,18 +95,38 @@ namespace Deployer
                     {
                         var splCmd = splRunCommand.Split('?');
                         if (splCmd.Length != 2) return false;
-                        var runCommand = await RunProcess(splCmd[0].ExtractEnvPath(), splCmd[1].ExtractEnvPath());
+                        var runCommand = await RunProcess(splCmd[0].ExtractEnvPath(), splCmd[1].ExtractEnvPath(), false, true);
                         if(runCommand == 0) runCommandCount++;
                     }
                     if (runCommandCount == splRunCommands.Length)
                         return true;
                     break;
+                case "RunCommandsAdmin":
+                    var splRunCommandsAdmin = target.Split('|');
+                    var runCommandAdminCount = 0;
+                    foreach (var splRunCommand in splRunCommandsAdmin)
+                    {
+                        var splCmd = splRunCommand.Split('?');
+                        if (splCmd.Length != 2) return false;
+                        var runCommand = await RunProcess(splCmd[0].ExtractEnvPath(), splCmd[1].ExtractEnvPath(), true, true);
+                        if (runCommand == 0) runCommandAdminCount++;
+                    }
+                    if (runCommandAdminCount == splRunCommandsAdmin.Length)
+                        return true;
+                    break;
+                case "RunAndWait":
+                    var splRun = target.Split(new[] { '|' }, 3);
+                    var runPath = splRun[0].ExtractEnvPath();
+                    if (File.Exists(runPath) && int.TryParse(splRun[2],out var runTimeOut))
+                    {
+                        await RunAndExit(runPath, splRun[1].ExtractEnvPath(), runTimeOut);
+                        return true;
+                    }
+                    break;
                 case "PerformMsiexec":
-                    var splMsi = target.Split(new[] { '|' }, 2);
-                    if (splMsi.Length != 2) return false;
-                    var filePathMsi = splMsi[0].ExtractEnvPath();
+                    var filePathMsi = target.ExtractEnvPath();
                     if (File.Exists(filePathMsi))
-                        return await RunMsiexec($"/i {filePathMsi} {splMsi[1].ExtractEnvPath()}") == 0;
+                        return await RunMsiexec($"{filePathMsi}") == 0;
                     break;
                 case "WriteToFile":
                     var splWrite = target.Split(new[] { '|' }, 2);
@@ -260,7 +281,6 @@ namespace Deployer
                 result = await HttpGet("https://pub.shonan-it.ac/deployer/config.json?t=" + Guid.NewGuid().ToString("N"), 3000);
             Config = !string.IsNullOrEmpty(result) ? SimpleJson.SimpleJson.DeserializeObject<DeployConfig>(result) : null;
         }
-
         public static async Task<string> HttpGet(string url, int timeout)
         {
             using (var client = new System.Net.Http.HttpClient())
@@ -274,7 +294,6 @@ namespace Deployer
                 return null;
             }
         }
-
         public static async Task<int> RunAtNewDesktop(string command)
         {
             using (var desktop = Onyeyiri.Desktop.CreateDesktop("sitdeployer"))
@@ -285,7 +304,6 @@ namespace Deployer
                 return p.ExitCode;
             }
         }
-
         public static async Task<int> RunProcess(string path, string param, bool runas = false, bool hide = false)
         {
             var process = new Process
@@ -306,26 +324,45 @@ namespace Deployer
             await process.WaitForExitAsync();
             return process.ExitCode;
         }
-
+        public static async Task RunAndExit(string path, string param, int timeout)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = path,
+                    Arguments = param,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                }
+            };
+            process.Start();
+            await Task.Delay(timeout);
+            process.Kill();
+            process.WaitForExit();
+        }
         public static async Task<int> RunMsiexec(string args)
         {
             var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = "msiexec",
-                    Arguments = args,
+                    FileName = "msiexec.exe",
+                    Arguments = $"/i {args} /passive /qn /norestart",
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
-                    CreateNoWindow = true
+                    CreateNoWindow = true,
+                    Verb = "runas"
                 }
             };
             process.Start();
             await process.WaitForExitAsync();
             return process.ExitCode;
         }
-
         public static async Task<bool> Ping(string ip)
         {
             try { 
@@ -339,7 +376,6 @@ namespace Deployer
                 return false;
             }
         }
-
         public static async Task<byte[]> DownloadFile(string url,int timeout = 30000)
         {
             using (var client = new System.Net.Http.HttpClient(new System.Net.Http.HttpClientHandler { AllowAutoRedirect = true }))
@@ -393,7 +429,6 @@ namespace Deployer
                 }
             }
         }
-
         public static bool CheckIfFileInEnvironment(string fileName)
         {
             var find = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User)?.Split(';').Select(s => Path.Combine(s, "gcc.exe")).FirstOrDefault(File.Exists) ?? Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine)?.Split(';').Select(s => Path.Combine(s, "gcc.exe")).FirstOrDefault(File.Exists);
